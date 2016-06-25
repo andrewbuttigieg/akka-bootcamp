46f0f241e6800020d3fb2df886c32bf555443d9a
andrew.buttigieg <andrew@guts.com>
2016-06-25 12:01:50 +0200

    Initial Commit

---
+++ Unit-1/DoThis/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    class Messages
+    {
+        // in Messages.cs
+        #region Neutral/system messages
+        /// <summary>
+        /// Marker class to continue processing.
+        /// </summary>
+        public class ContinueProcessing { }
+        #endregion
+
+        #region Success messages
+        #endregion
+
+        #region Error messages
+        #endregion
+    }
+}
